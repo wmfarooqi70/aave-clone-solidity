696c40cd7344230cf87ac6b0d12a11c5dbd337b4
Waleed Farooqi <wmfarooqi70@gmail.com>
2022-06-28 00:53:02 +0500

    removed client

--- client/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { SliderInput } from "./SliderInput"
-export { BalanceMsg } from "./BalanceMsg"
-export { ConnectionRequiredMsg } from "./ConnectionRequiredMsg"
--- client/src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useStakeTokens } from "./useStakeTokens";
-export { useStakingBalance } from "./useStakingBalance";
-export { useUnstakeTokens } from "./useUnstakeTokens";
